ba9ae845367dd2d565717d7f05d589ede1d0d005
Meristen <meristen_uskeev@mail.ru>
2024-04-24 14:47:13 +0300

    sliderfinish

--- src/pages/products/productList/ProductsList.jsx
+++ src/pages/products/productList/ProductsList.jsx
@@ -1,42 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { getProducts } from "../../../store/products/products.actions";
-// import Button from "../../../widgets/buttons/Button";
-// import Cards from "../../../widgets/productCard/Cards";
-// import styles from "./products.module.css";
-// import { useSearchParams } from "react-router-dom";
-// const ProductsList = () => {
-//   const dispatch = useDispatch();
-//   const [page, setPage] = useState(1);
-//   const [searchParams, setSearchParams] = useSearchParams();
-//   // console.log("qwert");
-//   useEffect(() => {
-//     setSearchParams({ page });
-//   }, [page]);
-
-//   useEffect(() => {
-//     dispatch(getProducts());
-//   }, [dispatch, searchParams]);
-
-//   const { products } = useSelector((state) => state.products);
-//   // console.log(products);
-//   return (
-//     <>
-//       <div className={styles.containerCard}>
-//         {products?.map((item) => (
-//           <Cards key={item.id} item={item} />
-//         ))}
-//       </div>
-//       <div>
-//         <Button onClick={() => setPage(page - 1)}>Prev</Button>
-//         <span>{page}</span>
-//         <Button onClick={() => setPage(page + 1)}>Next</Button>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default ProductsList;
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../../../store/products/products.actions";
@@ -46,7 +7,6 @@ import styles from "./products.module.css";
 import { useSearchParams } from "react-router-dom";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Sliders from "../../../widgets/slider/Sliders";
 import Poster from "../../../widgets/poster/Poster";
 
 const ProductsList = () => {
@@ -65,7 +25,8 @@ const ProductsList = () => {
   const { products } = useSelector((state) => state.products);
 
   return (
-    <>
+    <div className={styles.wraper}>
+      <Poster />
       <div className={styles.containerCard}>
         {products?.map((item) => (
           <div key={item.id}>
@@ -73,12 +34,12 @@ const ProductsList = () => {
           </div>
         ))}
       </div>
-      <div>
+      <div className={styles.pagination}>
         <Button onClick={() => setPage(page - 1)}>Prev</Button>
         <span>{page}</span>
         <Button onClick={() => setPage(page + 1)}>Next</Button>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/widgets/poster/Poster.jsx
+++ src/widgets/poster/Poster.jsx
@@ -1,7 +1,92 @@
 import React from "react";
 import styles from "./poster.module.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 const Poster = () => {
-  return <div></div>;
+  const settings = {
+    dots: true,
+    infinite: true, // бесконечный цикл слайдов
+    speed: 500, // скорость перехода
+    slidesToShow: 3, // сколько слайдов показывать за раз
+    slidesToScroll: 1, // сколько слайдов прокручивать за раз
+  };
+  return (
+    <Slider className={styles.slider} {...settings}>
+      <div className={styles.card}>
+        <img
+          className={styles.posterImg}
+          src="https://img.freepik.com/free-photo/wide-angle-shot-of-a-single-tree-growing-under-a-clouded-sky-during-a-sunset-surrounded-by-grass_181624-22807.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1713657600&semt=ais"
+          alt="logo"
+        />
+        <div className={styles.cardContent}>
+          <h2>Title</h2>
+          <p>janr</p>
+          <p>year</p>
+        </div>
+      </div>
+      <div className={styles.card}>
+        <img
+          className={styles.posterImg}
+          src="https://itc.ua/wp-content/uploads/2023/11/p0g7c2b6.jpg"
+          alt="logo"
+        />
+        <div className={styles.cardContent}>
+          <h2>Title</h2>
+          <p>janr</p>
+          <p>year</p>
+        </div>
+      </div>
+      <div className={styles.card}>
+        <img
+          className={styles.posterImg}
+          src="https://c8.alamy.com/comp/2K7P2W9/kung-fu-panda-po-the-panda-2K7P2W9.jpg"
+          alt="logo"
+        />
+        <div className={styles.cardContent}>
+          <h2>Title</h2>
+          <p>janr</p>
+          <p>year</p>
+        </div>
+      </div>
+      <div className={styles.card}>
+        <img
+          className={styles.posterImg}
+          src="https://upload.wikimedia.org/wikipedia/ru/b/b3/%D0%90%D0%B2%D0%B0%D1%82%D0%B0%D1%80_%D0%9F%D1%83%D1%82%D1%8C_%D0%B2%D0%BE%D0%B4%D1%8B_%D0%BF%D0%BE%D1%81%D1%82%D0%B5%D1%80.jpg"
+          alt="logo"
+        />
+        <div className={styles.cardContent}>
+          <h2>Title</h2>
+          <p>janr</p>
+          <p>year</p>
+        </div>
+      </div>
+      <div className={styles.card}>
+        <img
+          className={styles.posterImg}
+          src="https://fs.kinomania.ru/file/film/2/dc/2dcc1c90af0dbc6b26c42dfa2b3a92ae.jpeg"
+          alt="logo"
+        />
+        <div className={styles.cardContent}>
+          <h2>Title</h2>
+          <p>janr</p>
+          <p>year</p>
+        </div>
+      </div>
+      <div className={styles.card}>
+        <img
+          className={styles.posterImg}
+          src="https://play-lh.googleusercontent.com/NgLaEVvAL_blfvsZfyRc3A6AYPoCN90PIBJyvUJlul2OmasG3lZWpb2IhF62rkorLuU"
+          alt="logo"
+        />
+        <div className={styles.cardContent}>
+          <h2>Title</h2>
+          <p>janr</p>
+          <p>year</p>
+        </div>
+      </div>
+    </Slider>
+  );
 };
 
 export default Poster;
